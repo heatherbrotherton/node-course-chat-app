68c8db525d950ef68c411ad2e158cd49a55fb339
Heather Brotherton <heather.brotherton@salesforce.com>
2018-11-21 15:04:27 -0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const path= require('path');
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '../public');//double underscore __dirname
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
@@ -14,11 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
   socket.on('disconnect', () => {
-    console.log('User was disconnected.');
+    console.log('User was disconnected');
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
