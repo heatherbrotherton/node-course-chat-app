5cadaeae95264ae639f1ad009363b15c414ce345
Heather Brotherton <heather.brotherton@salesforce.com>
2018-11-29 10:39:05 -0500

    Fixes--mostly errors in index.html

--- public/js/index.js
+++ public/js/index.js
@@ -9,8 +9,8 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  var formattedTime = moment(message.createdAt).format('h:mm a')
-  var template =jQuery('#message-template').html();
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
   var html = Mustache.render(template, {
     text: message.text,
     from: message.from,
@@ -20,39 +20,38 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(html);
 });
 
-socket.on('newLoctionMessage', function (message) {
-    var formattedTime = moment(message.createdAt).format('h:mm a')
-    var template = jQuery('#location-message-template').html();
-    var html = Mustache.render(template, {
-      from: message.from,
-      url: message.url,
-      createdAt: formattedTime
-    });
+socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-    jQuery('#messages').append(html);
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-var messageTextBox = jQuery('[name=message]');
+  var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
     from: 'User',
-    text: messageTextBox.val()
-  }, function() {
-    messageTextBox.val('')
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('')
   });
 });
 
 var locationButton = jQuery('#send-location');
-locationButton.on('click', function() {
+locationButton.on('click', function () {
   if (!navigator.geolocation) {
-    return alert('Geolocation not supported by your browser.')
+    return alert('Geolocation not supported by your browser.');
   }
 
-locationButton.attr('disabled', 'disabled').text('Sending location...');
-
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
     locationButton.removeAttr('disabled').text('Send location');
@@ -60,8 +59,8 @@ locationButton.attr('disabled', 'disabled').text('Sending location...');
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
-  }, function (){
+  }, function () {
     locationButton.removeAttr('disabled').text('Send location');
-    alert('Unable to fetch location.')
+    alert('Unable to fetch location.');
   });
 });
